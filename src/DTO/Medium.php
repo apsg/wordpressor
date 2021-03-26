3dc116b912e86b08670f2b0e2d8d2342f781da99
Szymon Gackowski <szymon.gackowski@gmail.com>
2021-03-26 20:22:57 +0100

    Check for nulls

--- src/DTO/Medium.php
+++ src/DTO/Medium.php
@@ -17,27 +17,27 @@ class Medium
         return object_get($this->data, 'media_details.sizes.featured.source_url');
     }
 
-    public function full() : string
+    public function full() : ?string
     {
-        return object_get($this->data, 'media_details.sizes.full.source_url');
+        return object_get($this->data, 'media_details.sizes.full.source_url') ;
     }
 
-    public function thumbnail() : string
+    public function thumbnail() : ?string
     {
         return object_get($this->data, 'media_details.sizes.thumbnail.source_url');
     }
 
-    public function medium() : string
+    public function medium() : ?string
     {
         return object_get($this->data, 'media_details.sizes.medium.source_url');
     }
 
-    public function large() : string
+    public function large() : ?string
     {
         return object_get($this->data, 'media_details.sizes.large.source_url');
     }
 
-    public function getSize(string $size) : string
+    public function getSize(string $size) : ?string
     {
         return object_get($this->data, "media_details.sizes.{$size}.source_url");
     }
