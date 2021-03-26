942192687df097d092537e817faa64280fb58a32
Szymon Gackowski <szymon.gackowski@gmail.com>
2021-03-26 14:01:12 +0100

    fix commands without tags

--- src/Commands/ClearCacheCommand.php
+++ src/Commands/ClearCacheCommand.php
@@ -1,18 +1,17 @@
 <?php
 namespace Apsg\Wordpressor\Commands;
 
-use Apsg\Wordpressor\CacheHelper;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Cache;
 
 class ClearCacheCommand extends Command
 {
     protected $signature = 'wordpressor:clear';
-    protected $description = 'Clear Wordpressor\'s cache, i.e. after new post was added';
+    protected $description = 'Clear cache, i.e. after new post was added';
 
     public function handle()
     {
-        Cache::tags([CacheHelper::PREFIX])->clear();
+        Cache::flush();
 
         $this->info('Cleared');
     }
